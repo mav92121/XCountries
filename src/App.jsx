ac672a6df853d743784341e7cf2ab28577193087
devobase-ninja <dev.devobase@gmail.com>
2024-12-04 22:55:59 +0530

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,45 @@ import { useState } from "react";
 
 function App() {
   const [countries, setContries] = useState([]);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const url = `https://xcountries-backend.azurewebsites.net/all`;
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setContries(data))
-      .catch((error) => console.error(error));
+      .then((data) => {
+        setContries(data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
   }, []);
   return (
     <div className="grid-container">
-      {countries.map((country, i) => {
-        return (
-          <div
-            className="country-card"
-            key={i}
-            style={{
-              width: 150,
-              height: 120,
-              border: "1px solid black",
-              display: "inline-block",
-            }}
-          >
-            <img style={{ width: 70 }} src={country.flag} alt={country.abbr} />
-            <p>{country.name}</p>
-          </div>
-        );
-      })}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {!error &&
+        countries.map((country, i) => {
+          return (
+            <div
+              className="country-card"
+              key={i}
+              style={{
+                width: 150,
+                height: 120,
+                border: "1px solid black",
+                display: "inline-block",
+              }}
+            >
+              <img
+                style={{ width: 70 }}
+                src={country.flag}
+                alt={country.abbr}
+              />
+              <p>{country.name}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
